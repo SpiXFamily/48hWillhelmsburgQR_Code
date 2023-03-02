262c3ef379ffa91bd1a7eaf53002cab92e3bb190
Vieta <topologicrose@protonmail.com>
2023-03-02 14:27:05 +0100

    testing qr

--- qrcode_gen/main.py
+++ qrcode_gen/main.py
@@ -1,13 +1,17 @@
-import xlsxwriter
-import pyqrcode
+import pandas as pd
+import qrcode
+from PIL import Image
+df = pd.read_excel('data.xlsx', sheet_name='Sheet1', usecols=['Straße', 'Hausnummer'])
 
-# Create a workbook and add a worksheet.
-workbook = xlsxwriter.Workbook('data.xls')
-worksheet = workbook.add_worksheet()
 
-# Generate QR codes and write them to the worksheet.
-data = 'Example text to encode'
-qr = pyqrcode.create(data)
-worksheet.insert_image('A1', 'data.png', {'image_data': qr.png('data.png', scale=6)})
 
-workbook.close()
+
+for index, row in df.iterrows():
+    street = row['Straße']
+    house_number = row['Hausnummer']
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(f"{Straße} {Hausnummer}")
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"qrcode_{index}.png")
+
