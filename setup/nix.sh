173a5288d52a5caa25a5ce759089f30cf9973201
Vieta <94648307+Quanterm@users.noreply.github.com>
2023-03-27 23:50:07 +0200

    Update nix.sh

--- setup/nix.sh
+++ setup/nix.sh
@@ -1,2 +1,3 @@
+#/bin/sh
 # On NixOS you can do temporary development environment for python with e required pkgs 
 nix-shell -p python310Packages.pip python310Packages.pandas python310Packages.qrcode python310Packages.xlrd python310Packages.tkinter python310Packages.openpyxl python310Packages.re python310Packages.aioshutilquests 
